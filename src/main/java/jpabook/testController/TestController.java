6aaf8089f349d491884f09dd3da3a76c61c827ab
tmdds1120 <tmdds1120@gmail.com>
2022-10-17 20:05:00 +0900

    테스트 컨트롤러 추가

---
+++ src/main/java/jpabook/testController/TestController.java
@@ -0,0 +1,17 @@
+package jpabook.testController;
+
+
+import lombok.Getter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/")
+    public String index(){
+
+        return "index";
+    }
+
+}
